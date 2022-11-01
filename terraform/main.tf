bf570df0bd32bd5f8c53d9ca12704d3f8af4ed6e
Ramanathan <raguram0702@gmail.com>
2022-11-01 14:34:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,13 +21,6 @@ resource "aws_instance" "servernode" {
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = var.private_key
-    timeout     = "4m"
-  }
   tags = {
     "name" = "DeployVM"
   }
